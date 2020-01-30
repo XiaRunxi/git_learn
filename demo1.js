721346b2efd21933f2dff20d00695b3f4159ae4d
xiaojun <xiaorunxi@hotmail.com>
2020-01-30 13:58:27 +0800

    update css content and html content

--- demo1.js
+++ demo1.js
@@ -1,5 +1,5 @@
 setTimeout(function(){
-    document.body.innerText='I learning git';
-    document.body.style.fontSize='22px';
+   // document.body.innerText='I learning git';
+   // document.body.style.fontSize='22px';
     
 },3000)
